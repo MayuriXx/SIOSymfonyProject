bdab0cd3a3545b66554b329795f50e2791f356e4
HAYET Marc-Antoine <marcantoinehayet@gmail.com>
2017-04-13 10:34:10 +0200

    [Dev] Création des routes par défauts

--- src/LPSIO/PlateformeBundle/Controller/DefaultController.php
+++ src/LPSIO/PlateformeBundle/Controller/DefaultController.php
@@ -10,4 +10,14 @@ class DefaultController extends Controller
     {
         return $this->render('LPSIOPlateformeBundle:Default:index.html.twig');
     }
+
+    public function aboutAction()
+    {
+        return $this->render('LPSIOPlateformeBundle:Default:about.html.twig');
+    }
+
+    public function contactAction()
+    {
+        return $this->render('LPSIOPlateformeBundle:Default:contact.html.twig');
+    }
 }
