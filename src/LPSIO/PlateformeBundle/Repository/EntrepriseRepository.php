73b88e381688847d8487c8281171cc6638e7fb39
HAYET Marc-Antoine <marcantoinehayet@gmail.com>
2017-04-13 10:42:27 +0200

    [Dev] Génération des répertoires

---
+++ src/LPSIO/PlateformeBundle/Repository/EntrepriseRepository.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Windows
+ * Date: 13/04/2017
+ * Time: 10:38
+ */
+
+namespace LPSIO\PlateformeBundle\Repository;
+
+class EntrepriseRepository extends \Doctrine\ORM\EntityRepository
+{
+
+}
