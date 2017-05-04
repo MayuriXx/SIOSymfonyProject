8cc3267388780d33da68b30ebc532c154bea9f84
Marc-Antoine HAYET <marcantoinehayet@gmail.com>
2017-05-04 09:57:35 +0200

    [Dev] Développement des intefaces : formulaire de contact terminé.

--- src/LPSIO/PlateformeBundle/Controller/DefaultController.php
+++ src/LPSIO/PlateformeBundle/Controller/DefaultController.php
@@ -2,9 +2,16 @@
 
 namespace LPSIO\PlateformeBundle\Controller;
 
+use LPSIO\PlateformeBundle\Entity\Contact;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class DefaultController extends Controller
+
 {
     public function indexAction()
     {
@@ -18,7 +25,20 @@ class DefaultController extends Controller
 
     public function contactAction()
     {
-        return $this->render('LPSIOPlateformeBundle:Default:contact.html.twig');
+        $contact = new Contact();
+
+        $formBuilder = $this->get('form.factory')->createBuilder(FormType::class, $contact);
+
+        $formBuilder = $this->createFormBuilder($contact)
+            ->add('nom', TextType::class, array('label' => 'Nom'))
+            ->add('prenom', TextType::class, array('label' => 'Prénom'))
+            ->add('courriel', EmailType::class, array('label' => 'Courriel'))
+            ->add('message', TextareaType::class, array('label' => 'Message'))
+            ->add('save', SubmitType::class, array('label' => 'Envoyer'));
+
+        $form = $formBuilder->getForm();
+
+        return $this->render('LPSIOPlateformeBundle:Default:contact.html.twig', array('form' => $form->createView()));
     }
 
     public function inscriptionAction()
