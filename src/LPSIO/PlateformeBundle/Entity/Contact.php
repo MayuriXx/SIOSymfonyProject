6e95feea8d5ff1688ebb0648746c681e136997df
Marc-Antoine HAYET <marcantoinehayet@gmail.com>
2017-05-04 09:46:03 +0200

    [Dev] Génération de l'entité et du repository Contact (pour le formulaire Contact du site).

---
+++ src/LPSIO/PlateformeBundle/Entity/Contact.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace LPSIO\PlateformeBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Contact
+ *
+ * @ORM\Table(name="contact")
+ * @ORM\Entity(repositoryClass="LPSIO\PlateformeBundle\Repository\ContactRepository")
+ */
+class Contact
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nom", type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="prenom", type="string", length=255)
+     */
+    private $prenom;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="courriel", type="string", length=255)
+     */
+    private $courriel;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="message", type="string", length=255)
+     */
+    private $message;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nom
+     *
+     * @param string $nom
+     *
+     * @return Contact
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Get nom
+     *
+     * @return string
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * Set prenom
+     *
+     * @param string $prenom
+     *
+     * @return Contact
+     */
+    public function setPrenom($prenom)
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    /**
+     * Get prenom
+     *
+     * @return string
+     */
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+
+    /**
+     * Set courriel
+     *
+     * @param string $courriel
+     *
+     * @return Contact
+     */
+    public function setCourriel($courriel)
+    {
+        $this->courriel = $courriel;
+
+        return $this;
+    }
+
+    /**
+     * Get courriel
+     *
+     * @return string
+     */
+    public function getCourriel()
+    {
+        return $this->courriel;
+    }
+
+    /**
+     * Set message
+     *
+     * @param string $message
+     *
+     * @return Contact
+     */
+    public function setMessage($message)
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    /**
+     * Get message
+     *
+     * @return string
+     */
+    public function getMessage()
+    {
+        return $this->message;
+    }
+}
+
