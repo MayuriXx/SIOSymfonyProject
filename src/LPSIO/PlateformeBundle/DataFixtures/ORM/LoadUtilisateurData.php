cd216a8831daabe6ef57f116055568970b4597bf
Marc-Antoine HAYET <marcantoinehayet@gmail.com>
2017-05-12 13:55:12 +0200

    [Correction] Création d'un utilisateur ayant le rôle SUPER ADMIN dans la classe fixture.

--- src/LPSIO/PlateformeBundle/DataFixtures/ORM/LoadUtilisateurData.php
+++ src/LPSIO/PlateformeBundle/DataFixtures/ORM/LoadUtilisateurData.php
@@ -13,6 +13,7 @@ class LoadUtilisateurData implements FixtureInterface
     public function load(ObjectManager $manager)
     {
         $utilisateur = new Utilisateur();
+        $administrateur = new Utilisateur();
 
         $utilisateur->setNom("HAYET");
         $utilisateur->setPrenom("Marc-Antoine");
@@ -24,9 +25,22 @@ class LoadUtilisateurData implements FixtureInterface
         $utilisateur->setDateInscription(new \DateTime());
         $utilisateur->setPassword("130795");
         $utilisateur->setSalt('');
-        $utilisateur->setRoles(array('ROLE_ADMIN'));
+        $utilisateur->setRoles(array('ROLE_USER'));
+
+        $administrateur->setNom("BENAMEUR");
+        $administrateur->setPrenom("Nasser");
+        $administrateur->setCourriel("nasser.benameur@univ-valenciennes.fr ");
+        $administrateur->setUsername("nasser.benameur@univ-valenciennes.fr ");
+        $administrateur->setAdresse("n/r");
+        $administrateur->setVille("n/r");
+        $administrateur->setPays("FRANCE");
+        $administrateur->setDateInscription(new \DateTime());
+        $administrateur->setPassword("1234");
+        $administrateur->setSalt('');
+        $administrateur->setRoles(array('ROLE_SUPER_ADMIN'));
 
         $manager->persist($utilisateur);
+        $manager->persist($administrateur);
 
         $manager->flush();
     }
